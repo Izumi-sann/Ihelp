c0a886f576208a33b8ca9f37b32eb8bd88cd8daf
Izumi-sann <gambalessandro41@gmail.com>
2025-04-09 10:09:49 +0200

    integrazione database parte terza: la rimozione dei file inutili

--- Sito_Web/button.js
+++
@@ -1,7 +0,0 @@
-// SCRIPT PER CHIUDERE MENU' PRINCIPALE
-document.querySelector('.close-menu').addEventListener('click', () => {
-    const menuCheckbox = document.getElementById('menu-toggle');
-    menuCheckbox.checked = false; // Deseleziona la checkbox per chiudere il menu
-  });
-
-
